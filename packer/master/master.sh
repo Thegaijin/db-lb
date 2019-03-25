965e5ab6f71d8522c2777cd31eef1cdc133f28e1
Thegaijin <ssemagandavictor@gmail.com>
2019-03-25 23:19:32 +0300

    All files

---
+++ packer/master/master.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+setupEnvrionment () {
+  printf '===========================Setup the environment============================== \n'
+
+  cd ..
+  sudo apt-get update
+  sudo apt-get install -y postgresql
+  sudo apt-get install -y postgresql-client
+  sudo apt-get install -y postgresql-contrib
+}
+
+addPostgresUser () {
+  printf '===========================Setup the environment============================== \n'
+  sudo -u postgres psql -c "CREATE USER replica REPLICATION LOGIN ENCRYPTED PASSWORD 'lmsreplica';"
+}
+
+editPostgresConf () {
+  printf '===========================Edit Postgres Conf file============================== \n'
+  sudo rm -rf /etc/postgresql/9.4/main/postgresql.conf
+  sudo cp /Users/svictor/DevOps/GCP/LMS/db-lb/packer/master/postgresql.conf /etc/postgresql/9.5/main/postgresql.conf
+}
+
+run () {
+  setupEnvrionment
+  addPostgresUser
+  editPostgresConf
+}
+
+run
