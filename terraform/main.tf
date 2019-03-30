614c0b851f4aee3c3c28ff74bb36f8a28d1a4715
Thegaijin <ssemagandavictor@gmail.com>
2019-03-30 19:19:55 +0300

    Add terraform scripts

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,81 +1,88 @@
 # Specify the provider (GCP)
-provider “google” {
-  credentials = “${file(“../terraform-account.json”)}”
-  project = “andela-learning”
-  region = “europe-west1”
+provider "google" {
+  credentials = "${file("../shared/account-terraform.json")}"
+  project     = "andela-learning"
+  region      = "${var.region}"
 }
 
 # Create the master db instance
-resource "google_compute_instance" "db-lb" {
-  name = "psql-master"
+resource "google_compute_instance" "db-lb-master" {
+  name         = "thegaijin-test-master"
   machine_type = "n1-standard-1"
-  zone = "europe-west1-b"
+  zone         = "${var.zone}"
 
   boot_disk {
-      initialize_params {
+    initialize_params {
       image = "db-lb-master"
+    }
   }
-}
-network_interface {
-  network = "default"
-  access_config {
+
+  network_interface {
+    network = "default"
+
+    access_config {
       // Ephemeral IP
     }
   }
 }
 
-resource "google_compute_instance" "db-lb" {
-  name = "psql-slave1"
+resource "google_compute_instance" "db-lb-slave1" {
+  name         = "thegaijin-test-slave1"
   machine_type = "n1-standard-1"
-  zone = "europe-west1-b"
+  zone         = "${var.zone}"
 
   boot_disk {
-      initialize_params {
+    initialize_params {
       image = "db-lb-slaves"
+    }
   }
-}
-network_interface {
-  network = "default"
-  access_config {
+
+  network_interface {
+    network = "default"
+
+    access_config {
       // Ephemeral IP
     }
   }
 }
 
-resource "google_compute_instance" "db-lb" {
-  name = "psql-slave2"
+resource "google_compute_instance" "db-lb-slave2" {
+  name         = "thegaijin-test-slave2"
   machine_type = "n1-standard-1"
-  zone = "europe-west1-b"
+  zone         = "${var.zone}"
 
   boot_disk {
-      initialize_params {
+    initialize_params {
       image = "db-lb-slaves"
+    }
   }
-}
-network_interface {
-  network = "default"
-  access_config {
+
+  network_interface {
+    network = "default"
+
+    access_config {
       // Ephemeral IP
     }
   }
 }
 
 resource "google_compute_instance" "haproxy-db-lb" {
-  name = "haproxy"
+  name         = "thegaijin-test-haproxy"
   machine_type = "n1-standard-1"
-  zone = "europe-west1-b"
+  zone         = "${var.zone}"
 
   boot_disk {
-      initialize_params {
+    initialize_params {
       image = "haproxy-image"
-  }
-}
-network_interface {
-  network = "default"
-  network_ip = "${var.haproxy_internal_ip}"
-  access_config {
-    nat_ip: "${var.haproxy_external_ip}"
     }
+  }
 
+  network_interface {
+    network    = "default"
+    network_ip = "${var.haproxy_internal_ip}"
+
+    access_config {
+      nat_ip = "${var.haproxy_external_ip}"
+    }
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,19 @@
-variable "master_db_ip" {
+variable "region" {
   type    = "string"
-  default = "10.132.1.1/24"
+  default = "europe-west1"
 }
 
-variable "slave1_db_ip" {
+variable "zone" {
   type    = "string"
-  default = "10.132.1.2/24"
-}
-
-variable "slave2_db_ip" {
-  type    = "string"
-  default = "10.132.1.3/24"
+  default = "europe-west1-b"
 }
 
 variable "haproxy_internal_ip" {
   type    = "string"
-  default = "10.132.1.10/24"
+  default = "10.132.0.77"
 }
 
 variable "haproxy_external_ip" {
   type    = "string"
-  default = "35.250.111.24/24"
+  default = "35.205.225.11"
 }
